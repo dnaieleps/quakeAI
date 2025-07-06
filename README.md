acbe5c48b4bf7bec896cf0bf9f338f01d5544579
dnaieleps <dan.c.espiritu@gmail.com>
2025-07-06 16:30:50 -0700

    first ever commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Contributors: Daniel Espiritu, Ara Chobanyan, Matthew Yom, Jian Wang, Peng Tang
+
+In this project, we created a forecasting random forest decision tree ensemble that is able to predict the probability of an earthquake happening given a location, time, and magnitude
+
+All dependencies are included in the virtual environment '.venv', and so to be able to run this code all you have to do is download all of files within this project, activate the virtual environment using 
+'.venv/bin/activate', and then train the model by running the program.
---
+++ source/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib as plt
+import tensorflow as tf
+import pandas as pd
+import sklearn as sk
+
+# clean the .csv file on earthquake data found in the resources folder (pandas)
+
+
+# turn cleaned file into np.array to be passed into the input layer of model (numpy + pandas)
+
+
+# create the model architecture to be able for prediction forecasting  (tensorflow / pytorch)
+# **research has shown that random forest is best for predicting WHEN, and LSTM is best for predicting magnitude
+
+
+# train the model given the specific architecture (tensorflow, sklearn)
+
+
+# do error analysis and construct graphs of training loss, cv loss, test loss, epochs, etc. (matplotlib + sklearn)
+
+
+# predict (tensorflow)
